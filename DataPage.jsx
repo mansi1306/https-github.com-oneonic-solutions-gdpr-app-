08a7904df28e3615855d0200f694756646b4d859
mansi1306 <mansi.naliyapara@oneonic.com>
2024-09-25 11:24:40 +0530

    git-1

---
+++ DataPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataList from '../components/DataList';
+
+const DataPage = () => {
+    return (
+        <div>
+            <h1>Data Page</h1>
+            <DataList />
+        </div>
+    );
+};
+
+export default DataPage;
